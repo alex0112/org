49edf9ff3f752227f0bed97c47cf127739051e88
alex0112 <alarsen0112@gmail.com>
2017-05-11 23:11:31 -0600

    Regenerated projects/tickets with the proper parent/child relationship

---
+++ db/migrate/20170512051028_create_tickets.rb
@@ -0,0 +1,13 @@
+class CreateTickets < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tickets do |t|
+      t.integer :number
+      t.string :descr
+      t.string :type
+      t.string :status
+      t.references :project, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
